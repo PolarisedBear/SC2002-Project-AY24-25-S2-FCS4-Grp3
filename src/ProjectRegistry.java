96ed269c169346ca3035ba5d6c5d3adaed79b2ab
gwee008 <gerald.wee.hp@gmail.com>
2025-03-29 22:49:36 +0800

    Added ProjectRegistry class

--- src/ProjectRegistry.java
+++ src/ProjectRegistry.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProjectRegistry {
-    private static List<Project> allProjects;
-
-    private static void addProject(Project) {
-
+public class ProjectRegistry{
+    private static List<Project> allProjects = new ArrayList<>();
+    private static void addProject(Project project){
+        allProjects.add(project);
     }
+    public static List<Project> removeProject(Project project){
+        if(allProjects.remove(project)){
+            return allProjects;
+        }
+        return allProjects;
+    }
+    public static List<Project> getAllProjects(){
+        return allProjects;
+    }
+    public static List<Project> findEligibleProjects(Applicant applicant){
+        //
+    }
+
 }
