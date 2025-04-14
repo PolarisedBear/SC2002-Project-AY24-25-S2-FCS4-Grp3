6fadeef75331361cf7d1f675f9c2163fa4df367f
PolarisedBear <popoalert50@gmail.com>
2025-04-14 17:14:35 +0800

    Renamed file names

--- src/sg/com/ntu/group3/models/ExcelFileReader.java
+++ src/sg/com/ntu/group3/models/ExcelFileReader.java
@@ -1,9 +1,11 @@
 package sg.com.ntu.group3.models;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class ExcelFileReader {
     public static List<String[]> read(String exfilePath) throws IOException {
--- src/sg/com/ntu/group3/models/UserRepository.java
+++ src/sg/com/ntu/group3/models/UserRepository.java
@@ -10,17 +10,20 @@ import sg.com.ntu.group3.roles.HDBManager;
 import sg.com.ntu.group3.roles.HDBOfficer;
 
 public class UserRepository {
-    private static final String applicantfilepath = "ApplicantList.xlsx"; //read different types of users
-    private static final String officerfilepath = "OfficerList.xlsx";
-    private static final String managerfilepath = "ManagerList.xlsx";
+    private static final String applicantfilepath = "src/ApplicantList.csv"; //read different types of users
+    private static final String officerfilepath = "src/OfficerList.csv";
+    private static final String managerfilepath = "src/ManagerList.csv";
 
     public static List<Applicant> getAllApplicants() throws IOException { 
         List<String[]> rows = ExcelFileReader.read(applicantfilepath);
         List<Applicant> applicantList = new ArrayList<>();
+        boolean firstRow = true;
 
         for (String[] row : rows) {
+            if (firstRow) {firstRow = false; continue;}
             Applicant applicant = new Applicant(row[0], row[1], Integer.parseInt (row[2]), row[3], row[4]);
             applicantList.add(applicant);
+
         }
 
         return applicantList;
@@ -29,8 +32,10 @@ public class UserRepository {
     public static List<HDBOfficer> getAllOfficers() throws IOException {
         List<String[]> rows = ExcelFileReader.read(officerfilepath);
         List<HDBOfficer> officerList = new ArrayList<>();
+        boolean firstRow = true;
 
         for (String[] row : rows) {
+            if (firstRow) {firstRow = false; continue;}
             HDBOfficer officer = new HDBOfficer(row[0], row[1], Integer.parseInt (row[2]), row[3], row[4]);
             officerList.add(officer);
         }
@@ -41,8 +46,10 @@ public class UserRepository {
     public static List<HDBManager> getAllManagers() throws IOException {
         List<String[]> rows = ExcelFileReader.read(managerfilepath);
         List<HDBManager> managerList = new ArrayList<>();
+        boolean firstRow = true;
 
         for (String[] row : rows) {
+            if (firstRow) {firstRow = false; continue;}
             HDBManager manager = new HDBManager(row[0], row[1], Integer.parseInt (row[2]), row[3], row[4]);
             managerList.add(manager);
         }
